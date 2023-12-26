1906edac3380207246cb34a39cbc163b08fe8069
Jodmxx <108124413+Jodmxx@users.noreply.github.com>
2023-12-26 23:45:32 +0530

    Update mirror_leech.py

--- bot/modules/mirror_leech.py
+++ bot/modules/mirror_leech.py
@@ -219,7 +219,7 @@ async def _mirror_leech(client, message, isQbit=False, isLeech=False, sameDir=No
     if task_utilis_msg:
         error_msg.extend(task_utilis_msg)
     if error_msg:
-        final_msg = f'Hey, <b>{tag}</b>!\n'
+        final_msg = f'Heybc, <b>{tag}</b>!\n'
         for __i, __msg in enumerate(error_msg, 1):
             final_msg += f'\n<b>{__i}</b>: {__msg}\n'
         if error_button is not None:
