6f7b5e380aa3d90fe7a8b4da5ad13340e12fb08b
Jodmxx <108124413+Jodmxx@users.noreply.github.com>
2023-12-26 23:30:30 +0530

    Update mirror_leech.py

--- bot/modules/mirror_leech.py
+++ bot/modules/mirror_leech.py
@@ -214,7 +214,7 @@ async def _mirror_leech(client, message, isQbit=False, isLeech=False, sameDir=No
     error_msg = []
     error_button = None
     if await nsfw_precheck(message):
-    	  error_msg.extend(['NSFW detected'])
+    	  error_msg.extend(['NSFW detected bc'])
     task_utilis_msg, error_button = await task_utils(message)
     if task_utilis_msg:
         error_msg.extend(task_utilis_msg)
